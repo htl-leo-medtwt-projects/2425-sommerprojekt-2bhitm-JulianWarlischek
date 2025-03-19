c53dd74f9b53eb3318fc618d3d5a81104343684b
JulianWarlischek <j.warlischek@student.htl-leonding.ac.at>
2025-03-19 23:04:51 +0100

    finished training.html layout (small changes are coming soon)

--- project/site/script/script.js
+++ project/site/script/script.js
@@ -16,7 +16,15 @@ let TRAINING_ELEMENTS = {
     number_of_sections: 4,
     section_backgrounds: [
         '../images/plan_your_training.jpg',
-        '../images/start_your_training.jpg'
+        '../images/start_your_training.jpg',
+        '../images/learn_about_your_muscles.jpg',
+        '../images/stay_hydrated.avif'
+    ],
+    section_headlines: [
+        'Plan your training',
+        'Start your training',
+        'Learn about your muscles',
+        'Stay hydrated'
     ]
 }
 
@@ -29,16 +37,21 @@ function loadIndex() { }
 /**
  * Dynamic content of training.html
  */
+//style="background-image: url('${TRAINING_ELEMENTS.section_backgrounds[i]}');"
 function loadSections() {
     let temp_string = "";
 
-    for (let i = 0; i < TRAINING_ELEMENTS.number_of_sections - 2; i++) {
+    for (let i = 0; i < TRAINING_ELEMENTS.number_of_sections; i++) {
         temp_string += `
-        <div class="section-box" style="background-image: url('${TRAINING_ELEMENTS.section_backgrounds[i]}');">
+        <div class="section-box" id="section-${i + 1}">
+            <div class="section-overlay ${i % 2 === 0 ? 'white-overlay' : 'darkblue-overlay'}">
+            </div>
+            <img src="${TRAINING_ELEMENTS.section_backgrounds[i]}"/>
+            <div class="section-label-box ${i % 2 === 0 ? 'section-label-left' : 'section-label-right'}"><h3>${TRAINING_ELEMENTS.section_headlines[i]}</h3></div>
         </div>
         `
         console.log(temp_string);
-        
+
     }
 
     TRAINING_ELEMENTS.sections_output.innerHTML = temp_string;
