d634384db6f0738e836a31d4e35032c036e45f57
JulianWarlischek <j.warlischek@student.htl-leonding.ac.at>
2025-04-24 14:42:35 +0200

    add content (drink-log.html)

--- project/site/script/executable_drink-log.js
+++ project/site/script/executable_drink-log.js
@@ -6,3 +6,22 @@ function loadDrinkLogSettings() {
     SETTINGS.userImgPath = DRINK_LOG_ELEMENTS.userImgPath;
 }
 loadDrinkLogSettings()
+
+/**
+ * Opens the menu to set a hydration goal
+ */
+function openHydrationSetter() {
+    fadeIn('hydration-page-overlay', 'block');
+
+    const slider = document.getElementById('hydration-setter');
+    slider.style.transform = "translateY(0)";
+}
+
+/**
+ * Closes the menu to set a hydration goal
+ */
+function closeHydrationSetter() {
+    const slider = document.getElementById('hydration-setter');
+    slider.style.transform = "translateY(100%)";
+    fadeOut('hydration-page-overlay', 500);
+}
